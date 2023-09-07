1e82aea48631bd0488ad6add6d60ecc86eb4d15b
sharathkumaradhiseshan <143284298+sharathkumaradhiseshan@users.noreply.github.com>
2023-09-07 22:31:17 +0530

    Add files via upload

---
+++ e-commace/src/main/java/com/example/ecommace/ECommaceApplication.java
@@ -0,0 +1,13 @@
+package com.example.ecommace;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ECommaceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ECommaceApplication.class, args);
+	}
+
+}
---
+++ e-commace/src/test/java/com/example/ecommace/ECommaceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ecommace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ECommaceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
